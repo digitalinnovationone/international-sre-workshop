27a2507d09eeb1547ac132237bab1d66d2cf0fa1
Albert Tanure <tanure@live.com>
2024-02-24 12:28:54 +0100

    Add: Example terraform

---
+++ infra/infra-example/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.1.7, < 2.0.0"
+  required_providers {
+    azurerm = {
+      version = "~>3.47.0"
+      source  = "hashicorp/azurerm"
+    }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "~>1.2.24"
+    }
+  }
+
+  backend "remote" {
+    # Empty since we will configure this dynamically in GitHub Actions
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Make client_id, tenant_id, subscription_id and object_id variables
+data "azurerm_client_config" "current" {}
